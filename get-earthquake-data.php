1b438d491b7f13c85a4d716cc2cd8df5107cbdd8
kusa-mochi <whoatemyapplepie@gmail.com>
2021-02-27 12:53:20 +0900

    GETパラメータの値チェックを追加。

--- get-earthquake-data.php
+++ get-earthquake-data.php
@@ -5,16 +5,35 @@ $query = 'select * from earthquake_data where dt >= :min_dt and dt <= :max_dt';
 $min_date = $_GET['min_date'];
 $max_date = $_GET['max_date'];
 
-// GETパラメータチェック
+///// ▼GETパラメータチェック▼ /////
+// GETパラメータが渡されていない場合
+if(empty($min_date) || empty($max_date))
+{
+  throw new InvalidArgumentException("min_date and max_date must not be empty.");
+}
+// GETパラメータに渡された文字列のフォーマットが不正な場合
 $min_datetime = Datetime::createFromFormat('Y-m-d H:i:s', $min_date);
 $max_datetime = Datetime::createFromFormat('Y-m-d H:i:s', $max_date);
-$diff_datetime = $max_datetime->diff($min_datetime);
+if($min_datetime == false || $max_datetime == false)
+{
+  throw new InvalidArgumentException("The string in min_date and max_date must follow the datetime format 'Y-m-d H:i:s'.");
+}
+// GETパラメータに渡された年月日・時刻が不正な場合
+$valid_max_diff = "+1 day"; // min_dateとmax_dateに許容する最大日時差
+$tmp_min_datetime = Datetime::createFromFormat('Y-m-d H:i:s', $min_date);
+$tmp_min_datetime->modify($valid_max_diff);
+if($tmp_min_datetime < $max_datetime)
+{
+  throw new InvalidArgumentException("Max diff between min_date and max_date is ".$valid_max_diff);
+}
 // min_dateがmax_dateよりも遅れていない場合
+$diff_datetime = $max_datetime->diff($min_datetime);
 $invert_datetime = $diff_datetime->invert;
 if($invert_datetime != 1)
 {
-    throw new InvalidArgumentException("min_datetime and max_datetime must be min_datetime < max_datetime");
+    throw new InvalidArgumentException("min_date and max_date must be min_date < max_date.");
 }
+///// ▲GETパラメータチェック▲ /////
 
 try
 {
